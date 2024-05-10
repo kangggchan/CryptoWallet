2aabed149fc74c81f6ccf84e885014c9f3aea697
Bui Phuoc Khang <khang.buiphuoc@gmail.com>
2024-05-10 10:23:36 +0900

    Initial Commit

---
+++ CryptoWallet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CryptoWallet
+//
+//  Created by Khang Bùi Phước on 5/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
